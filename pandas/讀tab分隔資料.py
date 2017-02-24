9b2a7fc72fcdf7a42cb1106d29f8c4963f13ae30
林宏仁 <tom.hr.lin@gmail.com>
2017-02-24 11:19:20 +0800

    Add files via upload

---
+++ pandas/讀tab分隔資料.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+
+# 讀入資料, pandas.DataFrame
+df=pd.read_csv('testSet-NOT-linearSeparable.txt', 
+	sep='\t', 
+	names=['feature01', 'feature02', 'label'])
+
+print(df)
+print('-'*30);
+
+
+# 將 pandas.DataFrame 轉成 numpy.ndarray
+x=df.as_matrix()
+print(x)
+print(x[:,2].astype(int))
+print(type(x))
+print('-'*30);
